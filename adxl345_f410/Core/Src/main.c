9dc6a9213b2a3dd32db934dfef8f9c7bc879890a
Furkan Karagoz <fkaragoz14@gmail.com>
2023-01-01 18:39:20 +0300

    First debug operation.

--- adxl345_f410/Core/Src/main.c
+++ adxl345_f410/Core/Src/main.c
@@ -96,6 +96,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	  HAL_Delay(500);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
